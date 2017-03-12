bf5bc7642bc78247bad6827413f39b9f7fa17ce1
clinyong <clinyong@gmail.com>
2017-03-12 17:06:08 +0800

    enable to edit form

--- src/views/Setting/index.tsx
+++ src/views/Setting/index.tsx
@@ -4,9 +4,14 @@ import { KEY, ExerciseProps } from "../../utils/constants";
 import { NavHead } from "../../components/NavHead";
 const styles = require("./index.scss");
 
+interface WorkoutProps {
+    name: string;
+    id: number;
+}
+
 interface SettingState {
     exercises: ExerciseProps[];
-    currentWorkout: string;
+    currentWorkout: WorkoutProps;
     startDate: string;
 }
 
@@ -16,13 +21,37 @@ export class Setting extends React.Component<any, SettingState> {
 
         this.state = {
             exercises: [],
-            currentWorkout: '',
+            currentWorkout: {
+                name: '',
+                id: -1
+            },
             startDate: ''
         }
         this.handleConfirm = this.handleConfirm.bind(this)
     }
 
     handleConfirm() {
+        const settings = store.get(KEY);
+
+        if (settings) {
+            store.set(KEY, Object.assign({}, settings, this.state))
+        }
+
+        store.set(KEY, this.state)
+    }
+
+    handleChange(stateKey, val) {
+        this.setState({
+            [stateKey]: val
+        });
+    }
+
+    handleExerciseChange(index, val) {
+        const { exercises } = this.state
+        exercises[index].weight = val
+        this.setState({
+            exercises
+        })
     }
 
     componentDidMount() {
@@ -31,7 +60,7 @@ export class Setting extends React.Component<any, SettingState> {
             const { exercises, currentWorkout, startDate } = settings;
             this.setState({
                 exercises,
-                currentWorkout: currentWorkout.name,
+                currentWorkout,
                 startDate
             })
         }
@@ -44,13 +73,19 @@ export class Setting extends React.Component<any, SettingState> {
                 <NavHead title={'SETTING'} />
                 <ul className={styles.settings}>
                     {
-                        exercises.map(e => {
+                        exercises.map((e, index) => {
                             return (
-                                <li>
+                                <li key={`${e.name}${index}`}>
                                     <span>
                                         {e.name}:
                                     </span>
-                                    <input type="text" value={e.weight} />
+                                    <input
+                                        type="text"
+                                        value={e.weight}
+                                        onChange={
+                                            ev => this.handleExerciseChange(index, ev.target.value)
+                                        }
+                                    />
                                 </li>
                             )
                         })
@@ -59,13 +94,24 @@ export class Setting extends React.Component<any, SettingState> {
                         <span>
                             Current Workout:
                         </span>
-                        <input type="text" value={currentWorkout} />
+                        <input
+                            type="text"
+                            value={currentWorkout.name}
+                            onChange={
+                                e => this.setState({
+                                    currentWorkout: {
+                                        name: e.target.value,
+                                        id: e.target.value === 'a' ? 0 : 1
+                                    }
+                                })
+                            }
+                        />
                     </li>
                     <li>
                         <span>
                             Start Date:
                         </span>
-                        <input type="text" value={startDate} />
+                        <input type="text" value={startDate} onChange={e => this.setState({ startDate: e.target.value })} />
                     </li>
                 </ul>
                 <button className={styles.submit} onClick={this.handleConfirm}>
