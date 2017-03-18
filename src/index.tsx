7798781078831a64371403b79af0fbb89f84b2b6
clinyong <clinyong@gmail.com>
2017-03-18 15:23:42 +0800

    format code

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { App } from "./App";
 import "./index.scss";
 
-if (process.env.NODE_ENV === 'production') {
-  if ('serviceWorker' in navigator) {
-    require('serviceworker-webpack-plugin/lib/runtime').register();
+if (process.env.NODE_ENV === "production") {
+  if ("serviceWorker" in navigator) {
+    require("serviceworker-webpack-plugin/lib/runtime").register();
   }
 }
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById("root");
 ReactDOM.render(
   <App />,
   rootEl
