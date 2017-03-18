cd40e7360d57dade1b93aed7000917db2c2de567
clinyong <clinyong@gmail.com>
2017-03-18 13:08:26 +0800

    fix button ripple

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,16 +6,77 @@ interface ButtonProps {
     onClick?: React.EventHandler<React.MouseEvent<HTMLButtonElement>>;
 }
 
-export class Button extends React.Component<ButtonProps, any> {
+interface ButtonState {
+    showRipple: boolean;
+    rippleLeft: number;
+    rippleTop: number;
+    rippleWidth: number;
+}
+
+export class Button extends React.Component<ButtonProps, ButtonState> {
+    btn: HTMLButtonElement;
+    rippleEle: HTMLSpanElement;
+
+    constructor(props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            showRipple: false,
+            rippleLeft: 0,
+            rippleTop: 0,
+            rippleWidth: 0
+        };
+    }
+
+    handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+        const { onClick } = this.props;
+        const { left, top } = this.btn.getBoundingClientRect();
+        const { offsetWidth, offsetHeight } = this.btn;
+
+        const rippleWidth = offsetWidth > offsetHeight ? offsetWidth : offsetHeight;
+
+        const rippleLeft = e.clientX - left - (rippleWidth / 2);
+        const rippleTop = e.clientY - top - (rippleWidth / 2);
+
+        this.setState({
+            showRipple: false,
+            rippleLeft,
+            rippleTop,
+            rippleWidth
+        });
+
+        setTimeout(() => {
+            this.setState({
+                showRipple: true
+            });
+        }, 0);
+
+        if (onClick) {
+            onClick(e);
+        }
+    }
+
     render() {
-        const { children, className, onClick } = this.props;
+        const { children, className } = this.props;
+        const { showRipple, rippleLeft, rippleTop, rippleWidth } = this.state;
         let cn = styles.container;
         if (className) {
             cn = `${cn} ${className}`;
         }
+
+        let rippleCN = styles.ripple;
+        if (showRipple) {
+            rippleCN = `${rippleCN} ${styles.rippleEffect}`;
+        }
         return (
-            <button className={cn} onClick={onClick}>
+            <button className={cn} onClick={this.handleClick} ref={btn => this.btn = btn}>
                 {children}
+                <span
+                    className={rippleCN}
+                    style={{ left: rippleLeft, top: rippleTop, width: rippleWidth, height: rippleWidth }}
+                    ref={ele => this.rippleEle = ele}
+                ></span>
             </button>
         );
     }
