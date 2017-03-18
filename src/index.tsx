49fd3ab20e1be50e94a7d214ecca7aa681a7e36e
clinyong <clinyong@gmail.com>
2017-03-18 15:25:18 +0800

    fix bugs

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App";
+import { HashRouter as Router, Route } from "react-router-dom";
+import { Wrapper } from "./views/Wrapper";
 import "./index.scss";
 
 if (process.env.NODE_ENV === "production") {
   if ("serviceWorker" in navigator) {
     require("serviceworker-webpack-plugin/lib/runtime").register();
   }
+}
+
+function App() {
+  return (
+    <Router>
+      <Route path="/" component={Wrapper} />
+    </Router>
+  );
 }
 
 const rootEl = document.getElementById("root");
